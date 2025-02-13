d30d8a86242b63b2f645ef2b6d4b7f0195e93e18
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-02-13 15:46:16 -0700

    Generate Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_12_213400) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_13_165848) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
