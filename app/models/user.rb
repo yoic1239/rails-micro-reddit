e7c5b009d1ae55127eb6f2b5f021194fbf1b45f8
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-02-14 13:23:43 -0700

    Add association between Post and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true, length: { in: 4..12 }
   validates :password, presence: true, length: { in: 6..16 }
+
+  has_many :posts
 end
