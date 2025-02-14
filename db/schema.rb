75b30308c5e6e45b28307f006916d556eed0e7d9
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-02-14 12:17:10 -0700

    Add user_id in Post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_13_165848) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_13_234530) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_13_165848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
